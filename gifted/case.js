65c0d920f091f140addace8f0959ce6829a74ac7
Maurice Gift <ceo@giftedtech.web.id>
2025-04-02 15:22:01 +0300

    Support Cases

--- gifted/case.js
+++ gifted/case.js
@@ -4,21 +4,24 @@ module.exports = {
     async handleCases(m, { Gifted, text, command }) {
         try {
             switch (command) {
+                    
+                // case 1 ——————————————
+   
                 case 'thxto': {
                     let cap = `Special thanks to @mouricedevs, my developer. This project is just Amazing!`
                     Gifted.reply({ text: cap }, m)
                 }
                 break;
                 
-                // next case ——————————————
-                
+                // case 2 ——————————————
+        
                 case 'rate': {
                     let rate = Math.floor(Math.random() * 100)
             		Gifted.reply({ text: `Bot Rate : *${rate}%*`, parse_mode: 'Markdown' }, m)
                 }
                 break;
 
-                // next case ——————————————
+                // case 3 ——————————————
                 
                 case 'web': {
             		Gifted.reply({ text: 'Visit the Owner Website Here: https://www.giftedtech.web.id' }, m)
