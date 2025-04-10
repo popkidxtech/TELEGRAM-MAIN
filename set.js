e24d72d92f0952e2f0cdc490700310c455375e8a
popkidxtech <popkidbotdeployment@gmail.com>
2025-04-10 08:01:21 +0300

    Update set.js

--- set.js
+++ set.js
@@ -1,12 +1,12 @@
-global.prefix = '/'; // Set Your Preferred Prefix
-global.botName = 'GIFTED-MD'; // Bot Name
+global.prefix = '.'; // Set Your Preferred Prefix
+global.botName = 'POPKID-MD'; // Bot Name
 global.botVersion = '2.0.0';
-global.botPic = 'https://files.giftedtech.web.id/file/gifted-md.jpg'; // Can use path, can also use link
-global.botToken = '7267479440:AAH_RAZAOk-075P2WgjPSsjiGzqb_3urXrw'; // Replace With Your Bot Token (Get it from BotFather)
-global.ownerUsername = 'mouricedevs'; // Replace With Your Telegram Username(For Bot Owner)
+global.botPic = 'https://files.catbox.moe/cjwzcb.jpg'; // Can use path, can also use link
+global.botToken = '7886085442:AAE1OkqQbZwBzDof2sRHSiixdlNsPEFR2Ds'; // Replace With Your Bot Token (Get it from BotFather)
+global.ownerUsername = 'Popkidtech254'; // Replace With Your Telegram Username(For Bot Owner)
 global.timeZone = 'Africa/Nairobi'; // Your Time Zone
-global.ownerId = [5153324742]; // Replace With Your Owner ID(For Bot Owner), You Can Add More than 1
-global.footer = '*Powered by GiftedTech*';
+global.ownerId = [7966970227]; // Replace With Your Owner ID(For Bot Owner), You Can Add More than 1
+global.footer = '*Powered by PopkidXtech*';
 global.ytdlWeb = 'https://ytdl.giftedtech.web.id';
 global.ytdlApi = 'https://giftedtech-ytdl-api.hf.space';
 global.giftedApi = 'https://api.giftedtech.web.id/api'; // Gifted Api
@@ -14,9 +14,42 @@ global.giftedKey = 'gifted'; // Gifted Api Key
 global.maherApi = 'https://api.nexoracle.com'; // NexOracle Api
 global.maherKey = ''; // NexOracle Api Key
 global.lyffeApi = 'https://levanter.onrender.com'; // Lyffe Api
-global.giftedWaChannel = 'https://whatsapp.com/channel/0029Vb3hlgX5kg7G0nFggl0Y';
-global.giftedRepo = "https://github.com/mauricegift/telegram-bot";
-global.giftedApiRepo = "https://api.github.com/repos/mauricegift/telegram-bot";
+global.giftedWaChannel = 'global.prefix = '.'; // Set Your Preferred Prefix
+global.botName = 'POPKID-MD'; // Bot Name
+global.botVersion = '2.0.0';
+global.botPic = 'https://files.catbox.moe/cjwzcb.jpg'; // Can use path, can also use link
+global.botToken = '7886085442:AAE1OkqQbZwBzDof2sRHSiixdlNsPEFR2Ds'; // Replace With Your Bot Token (Get it from BotFather)
+global.ownerUsername = 'Popkidtech254'; // Replace With Your Telegram Username(For Bot Owner)
+global.timeZone = 'Africa/Nairobi'; // Your Time Zone
+global.ownerId = [7966970227]; // Replace With Your Owner ID(For Bot Owner), You Can Add More than 1
+global.footer = '*Powered by PopkidXtech*';
+global.ytdlWeb = 'https://ytdl.giftedtech.web.id';
+global.ytdlApi = 'https://giftedtech-ytdl-api.hf.space';
+global.giftedApi = 'https://api.giftedtech.web.id/api'; // Gifted Api
+global.giftedKey = 'gifted'; // Gifted Api Key
+global.maherApi = 'https://api.nexoracle.com'; // NexOracle Api
+global.maherKey = ''; // NexOracle Api Key
+global.lyffeApi = 'https://levanter.onrender.com'; // Lyffe Api
+global.giftedWaChannel = 'https://whatsapp.com/channel/0029VadQrNI8KMqo79BiHr3l';
+global.giftedRepo = "https://github.com/Popkiddevs/POPKID-XTECH";
+global.giftedApiRepo = "https://api.github.com/repos/popkidxrech/TELEGRAM-MAIN";
+
+global.giftechMess = {
+    internalerr: 'Internal Server Error!',
+    owner: 'Owner-Only Feature!',
+    group: 'Use in a Group!',
+    private: 'Use in a Private Chat!',
+    wait: 'Please Wait...',
+    done: 'Completed Successfully!',
+    error: 'Oops, an Error Occurred'
+};
+
+String.prototype.capitalize = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
+';
+global.giftedRepo = "https://github.com/Popkiddevs/POPKID-XTECH";
+global.giftedApiRepo = "https://api.github.com/repos/popkidxrech/TELEGRAM-MAIN";
 
 global.giftechMess = {
     internalerr: 'Internal Server Error!',
